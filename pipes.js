cf1f7dd97a9113e2620dfcb44725976e16cfe3d9
gotankersley <gotankersley@harding.edu>
2014-09-01 09:36:25 -0500

    Test cylinder as pipe segment

--- pipes.js
+++ pipes.js
@@ -5,6 +5,7 @@ var container, scene, camera, renderer, controls;
 var floor;
 
 init();
+var cyl;
 function init() {
 		
 	scene = new THREE.Scene;
@@ -31,17 +32,30 @@ function init() {
 	//Scene	
 	
 	//Lighting
+	var light = new THREE.PointLight(0xffffff);
+	light.position.set(0,10,0);
+	scene.add(light);
+	
 	var hemi = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.3);
 	hemi.position.set(0, 10, 0);
 	scene.add(hemi);	
 	
 	//Floor		
-	var floorGeometry = new THREE.BoxGeometry(100, 1, 100);
+	var floorGeometry = new THREE.PlaneGeometry(100, 100);
 	var floorMaterial = new THREE.MeshLambertMaterial({
 		color: 0xff0000,
+		side:THREE.DoubleSide
 	});
-	floor = new THREE.Mesh(floorGeometry, floorMaterial);			
+	floor = new THREE.Mesh(floorGeometry, floorMaterial);
+	floor.rotateX(Math.PI/2);
 	scene.add(floor);	
+		
+	//Cylinder
+	var cylGeo = new THREE.CylinderGeometry(1, 1, 1, 10, 1, false);
+	//var cylMat = new THREE.MeshNormalMaterial({wireframe:true});
+	var cylMat = new THREE.MeshNormalMaterial();
+	cyl = new THREE.Mesh(cylGeo, cylMat);
+	scene.add(cyl);
 		
 	render();
 
